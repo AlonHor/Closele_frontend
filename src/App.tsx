5f7b1d36143057a2ed21750d5d0fa7de0e0842e9
Alon <alon.horesh.08@gmail.com>
2022-10-17 22:13:09 +0300

    Make keyboard theme match the app

--- src/App.tsx
+++ src/App.tsx
@@ -306,14 +306,16 @@ function App() {
               "{space}"
             ],
           }}
-          buttonTheme={
-            [
-              {
-                class: "hg-red",
-                buttons: "a b c d e f g h i j k l m n o p q r s t u v w x y z {space} {bksp} {delete} {enter}"
-              }
-            ]
-          }
+        /*
+        buttonTheme={
+          [
+            {
+              class: "hg-red",
+              buttons: "a b c d e f g h i j k l m n o p q r s t u v w x y z {space} {bksp} {delete} {enter}"
+            }
+          ]
+        }
+        */
         />
       )}
       <p>{`Socket is ${isConnected ? 'connected' : 'disconnected'}.`}</p>
