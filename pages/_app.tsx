937661f8294db1e885baa295416154004608b166
Alon <alon.horesh.08@gmail.com>
2022-10-29 13:13:22 -0700

    Remove SSR again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Fragment } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+const NonSSRWrapper = (props: { children: any }) => {
+  <Fragment>{props.children}</Fragment>
+}
+
+const Wrapper = dynamic(() => Promise.resolve(NonSSRWrapper), {
+  ssr: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <Wrapper><Component {...pageProps} /></Wrapper>;
 }
